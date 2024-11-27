b46cba264c760892799fa70cad76e82c44379c08
Cod-Am <amrit.672004@gmail.com>
2024-11-27 15:46:33 +0530

    all done

--- server.py
+++ server.py
@@ -8,16 +8,126 @@ from nltk.corpus import stopwords
 from flask_cors import CORS
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def processing(corpus):
-    model = pickle.load(open('trained_model.pkl', 'rb'))
-    vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
-    lemmatizer=WordNetLemmatizer()
-    tokenized_document = word_tokenize(corpus)
-    filtered_document = [word for word in tokenized_document if word.lower() not in stopwords.words('english')]
-    lemmatized_document = [lemmatizer.lemmatize(document) for document in filtered_document]
-    corpus = vectorizer.transform(lemmatized_document)
-    corpus = corpus.toarray()
-    result = model.predict(corpus)
+import re
+import nltk
+import string
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer, WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Dense, LSTM, Embedding, Bidirectional
+
+#nltk.download("stopwords")
+stop_words = set(stopwords.words("english"))
+lemmatizer= WordNetLemmatizer()
+
+# Modelling
+# from sklearn.model_selection import train_test_split,KFold, GridSearchCV
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix, classification_report
+# from sklearn.pipeline import Pipeline
+# from sklearn.metrics import f1_score
+# from sklearn.svm import SVC
+
+#Lime
+from lime import lime_text
+from lime.lime_text import LimeTextExplainer
+from lime.lime_text import IndexedString,IndexedCharacters
+from lime.lime_base import LimeBase
+from lime.lime_text import explanation
+sns.set(font_scale=1.3)
+nltk.download('omw-1.4')
+
+
+def lemmatization(text):
+    lemmatizer= WordNetLemmatizer()
+
+    text = text.split()
+
+    text=[lemmatizer.lemmatize(y) for y in text]
+    
+    return " " .join(text)
+
+def remove_stop_words(text):
+
+    Text=[i for i in str(text).split() if i not in stop_words]
+    return " ".join(Text)
+
+def Removing_numbers(text):
+    text=''.join([i for i in text if not i.isdigit()])
+    return text
+
+def lower_case(text):
+    
+    text = text.split()
+
+    text=[y.lower() for y in text]
+    
+    return " " .join(text)
+
+def Removing_punctuations(text):
+    ## Remove punctuations
+    text = re.sub('[%s]' % re.escape("""!"#$%&'()*+,،-./:;<=>؟?@[\]^_`{|}~"""), ' ', text)
+    text = text.replace('؛',"", )
+    
+    ## remove extra whitespace
+    text = re.sub('\s+', ' ', text)
+    text =  " ".join(text.split())
+    return text.strip()
+
+def Removing_urls(text):
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
+
+def remove_small_sentences(df):
+    for i in range(len(df)):
+        if len(df.text.iloc[i].split()) < 3:
+            df.text.iloc[i] = np.nan
+            
+def normalize_text(df):
+    df.Text=df.Text.apply(lambda text : lower_case(text))
+    df.Text=df.Text.apply(lambda text : remove_stop_words(text))
+    df.Text=df.Text.apply(lambda text : Removing_numbers(text))
+    df.Text=df.Text.apply(lambda text : Removing_punctuations(text))
+    df.Text=df.Text.apply(lambda text : Removing_urls(text))
+    df.Text=df.Text.apply(lambda text : lemmatization(text))
+    return df
+
+def normalized_sentence(sentence):
+    sentence= lower_case(sentence)
+    sentence= remove_stop_words(sentence)
+    sentence= Removing_numbers(sentence)
+    sentence= Removing_punctuations(sentence)
+    sentence= Removing_urls(sentence)
+    sentence= lemmatization(sentence)
+    return sentence
+
+def processing(sentence):
+    model = tf.keras.models.load_model('Emotion Recognition From English text.h5')
+    le=pickle.load(open('le.pkl', 'rb'))
+    tokenizer = pickle.load(open('tokenizer.pkl', 'rb'))
+    sentence = normalized_sentence(sentence)
+    sentence = tokenizer.texts_to_sequences([sentence])
+    sentence = pad_sequences(sentence, maxlen=229, truncating='pre')
+    result = le.inverse_transform(np.argmax(model.predict(sentence), axis=-1))[0]
+    # proba =  np.max(model.predict(sentence))
     return result
 
 app = Flask(__name__)
